091ed6e805a43594fbe3ce9e0f91a0f6bedf9faa
xeyalamanov121-commits <xeyalamanov121@gmail.com>
2026-05-24 14:27:46 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,56 +1,57 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-let players = {};
+let rooms = {}; 
 
 io.on('connection', (socket) => {
-    socket.on('joinGame', (userData) => {
-        players[socket.id] = { id: socket.id, username: userData.username, x: 50, score: 0, isDead: false };
-        io.emit('updatePlayers', players);
-    });
-
-    socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            socket.broadcast.emit('playerMoved', { id: socket.id, x: data.x });
-        }
+    socket.on('joinGame', (data) => {
+        // Boş otaq tap və ya yarat
+        let roomName = Object.keys(rooms).find(r => rooms[r].players.length < 4 && rooms[r].status === 'waiting') || `room_${Math.floor(Date.now() / 1000)}`;
+        
+        if (!rooms[roomName]) rooms[roomName] = { players: [], status: 'waiting' };
+        
+        socket.join(roomName);
+        socket.room = roomName;
+        rooms[roomName].players.push({ id: socket.id, username: data.username, score: 0, isDead: false });
+        
+        // Otaq dolanda oyunu başlat
+        if (rooms[roomName].players.length >= 1) rooms[roomName].status = 'playing';
+        io.to(roomName).emit('roomStarted');
     });
 
     socket.on('updateScore', (score) => {
-        if (players[socket.id]) {
-            players[socket.id].score = score;
+        if (socket.room && rooms[socket.room]) {
+            let p = rooms[socket.room].players.find(x => x.id === socket.id);
+            if (p) p.score = score;
         }
     });
 
     socket.on('playerDied', () => {
-        if (players[socket.id]) {
-            players[socket.id].isDead = true;
-            io.emit('playerExploded', socket.id);
-            
-            // Hamı öldüsə qalibi elan et
-            let allDead = Object.values(players).every(p => p.isDead);
-            if (allDead) {
-                let winner = null;
-                let maxScore = -1;
-                Object.values(players).forEach(p => {
-                    if (p.score > maxScore) { maxScore = p.score; winner = p.username; }
-                });
-                io.emit('gameResult', `QALİB: ${winner} (${maxScore} xal)`);
+        if (socket.room && rooms[socket.room]) {
+            let p = rooms[socket.room].players.find(x => x.id === socket.id);
+            if (p) {
+                p.isDead = true;
+                io.to(socket.room).emit('playerExploded', socket.id);
+                
+                // Hamı yandısa qalibi elan et
+                if (rooms[socket.room].players.every(x => x.isDead)) {
+                    let winner = rooms[socket.room].players.reduce((prev, curr) => (prev.score > curr.score) ? prev : curr);
+                    io.to(socket.room).emit('gameResult', `🏆 QALİB: ${winner.username} (${winner.score} xal)!`);
+                    delete rooms[socket.room]; 
+                }
             }
         }
     });
 
     socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('playerLeft', socket.id);
+        if (socket.room && rooms[socket.room]) {
+            rooms[socket.room].players = rooms[socket.room].players.filter(p => p.id !== socket.id);
+        }
     });
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(3000);
